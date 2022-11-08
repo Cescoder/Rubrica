a96664e0da9f8dfdc8c2c0474397789db6607fb8
Francesco Scuriatti <97753097+Cescoder@users.noreply.github.com>
2022-11-08 13:22:52 +0100

    Update Telephone.java

--- Telephone.java
+++ Telephone.java
@@ -1,1 +1,18 @@
-class R
+public class Telephone{
+  //ATTRIBUTI
+  String number;
+  
+  //COSTRUTTORE
+  public Telephone(String number){
+    this.number = number;
+  }
+  
+  //Setters and Getters
+  public void setNumber(String number){
+    this.number = number;
+  }
+  
+  public String getNumber(){
+    return number;
+  }
+}
